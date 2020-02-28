61ef40b154fdfa175d10fb2194ee32531604c1e8
Okko Hakola <ohakola@c2r6p4.hive.fi>
2020-02-28 18:41:21 +0200

    Init

---
+++ mandelbrot.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mandelbrot.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/28 18:37:42 by ohakola           #+#    #+#             */
+/*   Updated: 2020/02/28 18:40:44 by ohakola          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MANDELBROT_H
+# define MANDELBROT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct		s_complex
+{
+	double	r;
+	double	i;
+}					t_complex;
+
+#endif
